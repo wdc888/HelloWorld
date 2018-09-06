f15b2c29673555c8b0aa896202375ee24bbe8d9a
wdc888 <1475706756@qq.com>
2018-09-06 08:44:09 +0800

    eclipse中提交HelloWorld项目到github中

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * helloWorld
+ * @author wangdongchao
+ * @time 2018/9/6
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+	}
+
+}
